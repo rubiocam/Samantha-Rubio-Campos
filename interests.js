9e566215553bd94608e9adcf7fb9582c6024b440
rubiocam <rubiocam@usc.edu>
2026-02-03 16:08:36 -0800

    adding photos and buttons for the slide show

--- interests.js
+++ interests.js
@@ -1,23 +1,49 @@
 var slideIndex = 0;
-carousel();
+showDivs(slideIndex);
 
-function carousel() {
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("slides");
+//   var y = document.getElementsByClassName("container");
+
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//     y[i].style.display = "none";
+//   }
+
+//   slideIndex++;
+//   if (slideIndex > x.length) {
+//     slideIndex = 1
+//   }
+  
+//   x[slideIndex-1].style.display = "block";
+//   y[slideIndex-1].style.display = "block"
+  
+//   setTimeout(carousel, 30000); // Change image every 30 seconds
+// }
+
+function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slides");
   var y = document.getElementsByClassName("container");
 
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-    y[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1
   }
 
-  slideIndex++;
-  if (slideIndex > x.length) {
-    slideIndex = 1
+  
+  if (n < 1) {
+    slideIndex = x.length;
+  } 
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
   
   x[slideIndex-1].style.display = "block";
-  y[slideIndex-1].style.display = "block"
-  
-  setTimeout(carousel, 30000); // Change image every 30 seconds
+  y[slideIndex-1].style.display = "block";
 }
